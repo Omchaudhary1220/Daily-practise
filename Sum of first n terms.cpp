456efe452b5b0ac7a50d3773550761c024bbbb0b
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-03 12:57:14 +0530

    Create Sum of first n terms.cpp
    the sum of series 13 + 23 + 33 + 43 + … till N-th term.

---
+++ Sum of first n terms.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+   inline long long cube(long long n){
+    return n*n*n;
+    }
+    long long sumOfSeries(long long N) {
+    if(N==0||N==1){
+        return N;
+    }
+    else{
+        return cube(N)+sumOfSeries(N-1);
+     }
+    }
+};
